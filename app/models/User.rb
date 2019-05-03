87ee6428068aa699ea711f84a45fd0ee3c8b42e5
Zubair <zubair.maqsood6@gmail.com>
2019-05-03 16:14:22 +0100

    Skeleton code for the Models

--- app/models/User.rb
+++ app/models/User.rb
@@ -1,18 +1,18 @@
 class User < ActiveRecord::Base
     has_many :wannawatch
 
-    # def log_in
-    # end 
+    def log_in
+    end 
 
 
-    # def log_out
-    # end 
+    def log_out
+    end 
 
-    # def add_a_watch 
-    # end 
+    def add_a_watch 
+    end 
 
-    # def return_watch_list 
-    # end 
+    def return_watch_list 
+    end 
 
 
 
