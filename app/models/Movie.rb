2d98cbb10cbca7af1ed9afcfbb7ed489541e9870
Zubair <zubair.maqsood6@gmail.com>
2019-05-03 16:11:12 +0100

    Model file skeleton

--- app/models/Movie.rb
+++ app/models/Movie.rb
@@ -1,3 +1,15 @@
 class Movie < ActiveRecord::Base
+    has_many :wannawatch 
+
+    def upcoming_movies #returns an array of all upcoming movies 
+    end 
+
+    def upcoming_movies_by_genre
+    end 
+
+    def upcoming_movies_by_series 
+    end 
+
+
 end
     
--- app/models/User.rb
+++ app/models/User.rb
@@ -1,3 +1,20 @@
 class User < ActiveRecord::Base
+    has_many :wannawatch
+
+    # def log_in
+    # end 
+
+
+    # def log_out
+    # end 
+
+    # def add_a_watch 
+    # end 
+
+    # def return_watch_list 
+    # end 
+
+
+
 end
     
